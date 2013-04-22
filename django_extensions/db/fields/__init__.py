3a36f5b574b4f2af4cfa35a3731a6fb94851f5de
Aleksandr Vladimirskiy <aleksandr@butchershopcreative.com>
2013-04-22 13:59:25 -0700

    six expects a string or buffer

--- django_extensions/db/fields/__init__.py
+++ django_extensions/db/fields/__init__.py
@@ -277,7 +277,7 @@ class UUIDField(CharField):
             return value
         else:
             if self.auto and not value:
-                value = six.u(self.create_uuid())
+                value = six.u(str(self.create_uuid()))
                 setattr(model_instance, self.attname, value)
         return value
 
